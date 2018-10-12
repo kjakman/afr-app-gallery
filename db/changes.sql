4b48a75238f35911a50b13b8339760d4c72dd9a8
Dci <dci@email.com>
2018-10-12 13:31:46 +0000

    Filter Exhibitions

--- db/changes.sql
+++ db/changes.sql
@@ -10,3 +10,16 @@ ADD `exhibition_page_title` varchar(255) NOT NULL AFTER `exhibition_menu_title`;
 
 ALTER TABLE `minisite`
 ADD `exhibition_type` varchar(64) NOT NULL;
+
+ALTER TABLE `minisite`
+DROP `exhibition_type`;
+
+ALTER TABLE `minisite`
+ADD `show_gallery` tinyint(1) NULL DEFAULT '0' AFTER `exhibition_page_title`,
+ADD `show_artfair` tinyint(1) NULL DEFAULT '0' AFTER `show_gallery`,
+ADD `show_others` tinyint(1) NULL DEFAULT '0' AFTER `show_artfair`,
+ADD `show_past` tinyint(1) NULL DEFAULT '0' AFTER `show_others`,
+ADD `show_current` tinyint(1) NULL DEFAULT '0' AFTER `show_past`,
+ADD `show_future` tinyint(1) NULL DEFAULT '0' AFTER `show_current`,
+ADD `show_latest` tinyint(1) NULL DEFAULT '1' AFTER `show_future`;
+ADD `show_recent` tinyint(1) NULL DEFAULT '1' AFTER `show_latest`;
