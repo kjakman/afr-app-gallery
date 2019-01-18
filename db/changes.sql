169b3586506d9641f3ecb35a2592f4e4a305c28b
Dci <dci@email.com>
2019-01-18 13:50:40 +0000

    filters, CV, on page filters

--- db/changes.sql
+++ db/changes.sql
@@ -277,6 +277,49 @@ ADD `contact` text  NOT NULL;
 ALTER TABLE `user_profile`
 ADD `show_awards` tinyint(1) NOT NULL DEFAULT '1';
 
+# Jan -18
 
+ALTER TABLE `user_profile`
+DROP `facebook`,
+DROP `instagram`,
+DROP `twitter`,
+DROP `show_awards`;
+
+ALTER TABLE `user_profile`
+ADD `solo_shows_` text COLLATE 'utf8_general_ci' NOT NULL,
+ADD `group_shows_` text COLLATE 'utf8_general_ci' NOT NULL ,
+ADD `press_` text COLLATE 'utf8_general_ci' NOT NULL ,
+ADD `show_data` text COLLATE 'utf8_general_ci' NOT NULL;
+
+INSERT INTO `minisite_page_type` (`id`, `name`)
+SELECT '40', 'contact' from dual
+WHERE NOT EXISTS (SELECT * FROM `minisite_page_type` WHERE `id` = '40');
+
+ALTER TABLE `media_collection`
+ADD `about_title` varchar(100) NOT NULL DEFAULT ;
+
+CREATE TABLE `profile_media_map` (
+  `id` int(8) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `media_id` int(8) NOT NULL,
+  `site_id` int(11) NULL,
+  `artist_id` int(11) NULL,
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `curator_id` int(11) NULL,
+  `original_year` varchar(4) NOT NULL DEFAULT '',
+  `original_technique` varchar(255) NOT NULL DEFAULT '',
+  `artist_name` varchar(50) NOT NULL DEFAULT '',
+  `curator_name` varchar(50) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `artwork_id` int(11) NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+);
+
+ALTER TABLE `media_collection`
+ADD `filter` tinyint(1) NOT NULL DEFAULT '1';
+
+INSERT INTO `db_sequence` (`seq_name`, `nextid`)
+SELECT 'profile_media_map', '100' from dual
+WHERE NOT EXISTS (SELECT * FROM `db_sequence` WHERE `seq_name` = 'profile_media_map');
 
 
