666ee323f5f0ff3fb93ab4cba3c83b4717c9c8d3
Dci <dci@email.com>
2019-01-28 12:52:20 +0000

    changes.sql

--- db/changes.sql
+++ db/changes.sql
@@ -276,13 +276,58 @@ ADD `social` varchar(128) NOT NULL DEFAULT '';
 ALTER TABLE `user_profile`
 ADD `gallery_name` varchar(128) NOT NULL DEFAULT '';
 
-ALTER TABLE `user_profile` add `facebook` varchar(64) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '';
-ALTER TABLE `user_profile` add `instagram` varchar(64) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '';
-ALTER TABLE `user_profile` add `twitter` varchar(64) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '';
-ALTER TABLE `user_profile` ADD `artist` tinyint(1) NOT NULL DEFAULT '1';
+#ALTER TABLE `user_profile` add `facebook` varchar(64) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '';
+#ALTER TABLE `user_profile` add `instagram` varchar(64) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '';
+#ALTER TABLE `user_profile` add `twitter` varchar(64) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '';
+#ALTER TABLE `user_profile` ADD `artist` tinyint(1) NOT NULL DEFAULT '1';
 
-update minisite_page set data='{"layout":"1","show_recent":"1","show_past":"1","show_current":"1","show_future":"1","show_latest":"1","show_gallery":"1","show_artfair":"1","show_others":"1"}' where id=100;
-update user_profile set gallery_name='Stigter Van Doesburg' where id=2389;
-update user_profile set working_hours='{"Monday":"0:00-0:00","Tuesday":"0:00-0:00","Wednesday":"11:00-18:00","Thursday":"11:00-18:00","Friday":"11:00-18:00","Saturday":"13:00-18:00","Sunday":"0:00-0:00"}' where id=2389;
-update user_profile set facebook='https://www.facebook.com/stigtervandoesburg/' where id=2389;
-update user_profile set instagram='https://www.instagram.com/explore/locations/285426524/stigter-va' where id=2389;
+#update minisite_page set data='{"layout":"1","show_recent":"1","show_past":"1","show_current":"1","show_future":"1","show_latest":"1","show_gallery":"1","show_artfair":"1","show_others":"1"}' where id=100;
+#update user_profile set gallery_name='Stigter Van Doesburg' where id=2389;
+#update user_profile set working_hours='{"Monday":"0:00-0:00","Tuesday":"0:00-0:00","Wednesday":"11:00-18:00","Thursday":"11:00-18:00","Friday":"11:00-18:00","Saturday":"13:00-18:00","Sunday":"0:00-0:00"}' where id=2389;
+#update user_profile set facebook='https://www.facebook.com/stigtervandoesburg/' where id=2389;
+#update user_profile set instagram='https://www.instagram.com/explore/locations/285426524/stigter-va' where id=2389;
+
+# Jan -18
+
+ALTER TABLE `user_profile`
+DROP `facebook`,
+DROP `instagram`,
+DROP `twitter`,
+DROP `show_awards`;
+
+ALTER TABLE `user_profile`
+ADD `solo_shows_` text COLLATE 'utf8_general_ci' NOT NULL,
+ADD `group_shows_` text COLLATE 'utf8_general_ci' NOT NULL ,
+ADD `press_` text COLLATE 'utf8_general_ci' NOT NULL ,
+ADD `show_data` text COLLATE 'utf8_general_ci' NOT NULL;
+
+INSERT INTO `minisite_page_type` (`id`, `name`)
+SELECT '40', 'contact' from dual
+WHERE NOT EXISTS (SELECT * FROM `minisite_page_type` WHERE `id` = '40');
+
+ALTER TABLE `media_collection`
+ADD `about_title` varchar(100) NOT NULL DEFAULT ;
+
+CREATE TABLE `profile_media_map` (
+  `id` int(8) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `media_id` int(8) NOT NULL,
+  `site_id` int(11) NULL,
+  `artist_id` int(11) NULL,
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `curator_id` int(11) NULL,
+  `original_year` varchar(4) NOT NULL DEFAULT '',
+  `original_technique` varchar(255) NOT NULL DEFAULT '',
+  `artist_name` varchar(50) NOT NULL DEFAULT '',
+  `curator_name` varchar(50) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `artwork_id` int(11) NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+);
+
+ALTER TABLE `media_collection`
+ADD `filter` tinyint(1) NOT NULL DEFAULT '1';
+
+INSERT INTO `db_sequence` (`seq_name`, `nextid`)
+SELECT 'profile_media_map', '100' from dual
+WHERE NOT EXISTS (SELECT * FROM `db_sequence` WHERE `seq_name` = 'profile_media_map');
